296c21311ef1efbcd2d7a661ef5393481fe59549
cakekxtx <lucikatefrancine7@gmail.com>
2025-04-24 15:31:37 -0400

    finished layout and work on BookmarksAdapter.kt

--- app/src/main/java/com/example/tmclone/BookmarksAdapter.kt
+++ app/src/main/java/com/example/tmclone/BookmarksAdapter.kt
@@ -3,24 +3,28 @@ package com.example.tmclone
 import android.content.Intent
 import android.net.Uri
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class SuggestEventsAdapter (private val events: ArrayList<Event>):
-	RecyclerView.Adapter<SuggestEventsAdapter.MyViewHolder>(){
+class BookmarksAdapter (private val events: ArrayList<Event>):
+	RecyclerView.Adapter<BookmarksAdapter.MyViewHolder>(){
+
 
 	inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-		val imageButton = itemView.findViewById<ImageButton>(R.id.eventImageButton)
-		val eventName = itemView.findViewById<TextView>(R.id.suggestedEventName_textview)
-		var ticketurl = ""
+		val eventImage = itemView.findViewById<ImageButton>(R.id.bookmarkEvent_Image)
+		val eventNameTextView = itemView.findViewById<TextView>(R.id.bookmarkEventName_textview)
+		val eventTicketButton = itemView.findViewById<Button>(R.id.bookmarkTicketLink_button)
+
+		var ticketurl = " "
 
 		init{
-			imageButton.setOnClickListener{
+			eventTicketButton.setOnClickListener{
 				val context = itemView.context
 				val browserIntent = Intent(Intent.ACTION_VIEW)
 				if(ticketurl.isNotEmpty()){
@@ -32,31 +36,31 @@ class SuggestEventsAdapter (private val events: ArrayList<Event>):
 				}
 			}
 		}
-
-
 	}
 
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-		val view = LayoutInflater.from(parent.context).inflate(R.layout.suggest_event_layout, parent, false)
+		val view = LayoutInflater.from(parent.context)
+			.inflate(R.layout.bookmarks_layout, parent, false)
 		return MyViewHolder(view)
+	}
+
+	override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+		val currItem = events[position]
+		val context = holder.itemView.context
+
+		holder.ticketurl = currItem.ticketLink.toString()
+		holder.eventNameTextView.text = currItem.name
+
+		val highestQualityImage = currItem.images.maxByOrNull { it.width*it.height }
+
+		Glide.with(context)
+			.load(highestQualityImage?.url)
+			.placeholder(R.drawable.ic_launcher_background)
+			.into(holder.eventImage)
 	}
 
 	override fun getItemCount(): Int {
 		return events.size
 	}
 
-	override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-		val currItem = events[position]
-		val context = holder.itemView.context
-		holder.ticketurl = currItem.ticketLink.toString()
-		holder.eventName.text = currItem.name
-		val highestQualityImage = currItem.images.maxByOrNull {
-			it.width*it.height
-		}
-
-		Glide.with(context)
-			.load(highestQualityImage?.url)
-			.placeholder(R.drawable.ic_launcher_background)
-			.into(holder.imageButton)
-	}
 }
