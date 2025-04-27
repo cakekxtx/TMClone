945398b6dd3613c0eea997a0fb339448276682de
cakekxtx <lucikatefrancine7@gmail.com>
2025-04-27 19:55:12 -0400

    fix profile ui

--- app/src/main/java/com/example/tmclone/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/tmclone/ui/profile/ProfileFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -36,7 +37,7 @@ class ProfileFragment : Fragment() {
 	lateinit var profileName: TextView
 	lateinit var profileEmail: TextView
 	lateinit var changePasswordButton: Button
-	lateinit var logoutButton: Button
+	lateinit var logoutButton: ImageButton
 	lateinit var deleteAccountButton: Button
 
 	@SuppressLint("MissingInflatedId")
